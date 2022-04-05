0e4a35fd5a40f5ef031d71b1bc6c10ca1c96e64a
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2022-04-05 17:58:12 +0100

    Added CI and updated tgui

--- dependencies.sh
+++ dependencies.sh
@@ -8,7 +8,8 @@ export BYOND_MAJOR="513"
 export BYOND_MINOR="1542"
 
 #node version
-export NODE_VERSION=12
+export NODE_VERSION=14
+export NODE_VERSION_PRECISE=14.16.1
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.5
--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -47,28 +47,231 @@ export class Changelog extends Component {
       selectedDate: '',
       selectedIndex: 0,
     };
+    this.dateChoices = [];
   }
 
   setData(data) {
     this.setState({ data });
   }
 
+  setSelectedDate(selectedDate) {
+    this.setState({ selectedDate });
+  }
+
   setSelectedIndex(selectedIndex) {
     this.setState({ selectedIndex });
   }
+
+  getData = (date, attemptNumber = 1) => {
+    const { act } = useBackend(this.context);
+    const self = this;
+    const maxAttempts = 6;
+
+    if (attemptNumber > maxAttempts) {
+      return this.setData('Failed to load data after ' + maxAttempts + ' attempts');
+    }
+
+    act('get_month', { date });
+
+    fetch(resolveAsset(date + '.yml'))
+      .then(async changelogData => {
+        const result = await changelogData.text();
+        const errorRegex = /^Cannot find/;
+
+        if (errorRegex.test(result)) {
+          const timeout = 50 + attemptNumber * 50;
+
+          self.setData(
+            'Loading changelog data' + '.'.repeat(attemptNumber + 3)
+          );
+          setTimeout(() => {
+            self.getData(date, attemptNumber + 1);
+          }, timeout);
+        } else {
+          self.setData(yaml.load(result, { schema: yaml.CORE_SCHEMA }));
+        }
+      });
+  }
+
+  componentDidMount() {
+    const { data: { dates = [] } } = useBackend(this.context);
+
+    if (dates) {
+      dates.forEach(
+        date => this.dateChoices.push(dateformat(date, 'mmmm yyyy', true))
+      );
+      this.setSelectedDate(this.dateChoices[0]);
+      this.getData(dates[0]);
+    }
+  }
+
   render() {
-    const { data, selectedIndex } = this.state;
-    const { data: { raw_changelog } } = useBackend(this.context);
+    const { data, selectedDate, selectedIndex } = this.state;
+    const { data: { dates } } = useBackend(this.context);
+    const { dateChoices } = this;
+
+    const dateDropdown = dateChoices.length > 0 && (
+      <Stack mb={1}>
+        <Stack.Item>
+          <Button
+            className="Changelog__Button"
+            disabled={selectedIndex === 0}
+            icon={'chevron-left'}
+            onClick={() => {
+              const index = selectedIndex - 1;
+
+              this.setData('Loading changelog data...');
+              this.setSelectedIndex(index);
+              this.setSelectedDate(dateChoices[index]);
+              window.scrollTo(
+                0,
+                document.body.scrollHeight
+                || document.documentElement.scrollHeight
+              );
+              return this.getData(dates[index]);
+            }} />
+        </Stack.Item>
+        <Stack.Item>
+          <Dropdown
+            displayText={selectedDate}
+            options={dateChoices}
+            onSelected={value => {
+              const index = dateChoices.indexOf(value);
+
+              this.setData('Loading changelog data...');
+              this.setSelectedIndex(index);
+              this.setSelectedDate(value);
+              window.scrollTo(
+                0,
+                document.body.scrollHeight
+                || document.documentElement.scrollHeight
+              );
+              return this.getData(dates[index]);
+            }}
+            selected={selectedDate}
+            width={'150px'} />
+        </Stack.Item>
+        <Stack.Item>
+          <Button
+            className="Changelog__Button"
+            disabled={selectedIndex === dateChoices.length - 1}
+            icon={'chevron-right'}
+            onClick={() => {
+              const index = selectedIndex + 1;
+
+              this.setData('Loading changelog data...');
+              this.setSelectedIndex(index);
+              this.setSelectedDate(dateChoices[index]);
+              window.scrollTo(
+                0,
+                document.body.scrollHeight
+                || document.documentElement.scrollHeight
+              );
+              return this.getData(dates[index]);
+            }} />
+        </Stack.Item>
+      </Stack>
+    );
 
     const header = (
       <Section>
-        <h1>Colonial Marines - Space Station 13</h1>
+        <h1>Traditional Games Space Station 13</h1>
         <p>
           <b>Thanks to: </b>
-          Baystation 12, /tg/station, /vg/station, NTstation, CDK Station devs,
+          Baystation 12, /vg/station, NTstation, CDK Station devs,
           FacepunchStation, GoonStation devs, the original Space Station 13
           developers, Invisty for the title image and the countless others who
           have contributed to the game, issue tracker or wiki over the years.
+        </p>
+        <p>
+          {'Current project maintainers can be found '}
+          <a href="https://github.com/tgstation?tab=members">
+            here
+          </a>
+          {', recent GitHub contributors can be found '}
+          <a href="https://github.com/tgstation/tgstation/pulse/monthly">
+            here
+          </a>.
+        </p>
+        <p>
+          {'You can also join our discord '}
+          <a href="https://tgstation13.org/phpBB/viewforum.php?f=60">
+            here
+          </a>.
+        </p>
+        {dateDropdown}
+      </Section>
+    );
+
+    const footer = (
+      <Section>
+        {dateDropdown}
+        <h3>CM 13 Development Team</h3>
+        <p>
+          Traditional Games Space Station 13 is thankful to the
+          GoonStation 13 Development Team for its work on the game up to the
+          {' r4407 release. The changelog for changes up to r4407 can be seen '}
+          <a href="https://wiki.ss13.co/Changelog#April_2010">
+            here
+          </a>
+          .
+        </p>
+        <p>
+          {'Except where otherwise noted, Goon Station 13 is licensed under a '}
+          <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">
+            Creative Commons Attribution-Noncommercial-Share Alike 3.0 License
+          </a>
+          {'. Rights are currently extended to '}
+          <a href="http://forums.somethingawful.com/">SomethingAwful Goons</a>
+          {' only.'}
+        </p>
+        <h3>Colonial Marines Space Station 13 License</h3>
+        <p>
+          {'All code after '}
+          <a href={'https://github.com/tgstation/tgstation/commit/'
+            + '333c566b88108de218d882840e61928a9b759d8f'}>
+            commit 333c566b88108de218d882840e61928a9b759d8f on 2014/31/12
+            at 4:38 PM PST
+          </a>
+          {' is licensed under '}
+          <a href="https://www.gnu.org/licenses/agpl-3.0.html">
+            GNU AGPL v3
+          </a>
+          {'. All code before that commit is licensed under '}
+          <a href="https://www.gnu.org/licenses/gpl-3.0.html">
+            GNU GPL v3
+          </a>
+          {', including tools unless their readme specifies otherwise. See '}
+          <a href="https://github.com/tgstation/tgstation/blob/master/LICENSE">
+            LICENSE
+          </a>
+          {' and '}
+          <a
+            href="https://github.com/tgstation/tgstation/blob/master/GPLv3.txt">
+            GPLv3.txt
+          </a>
+          {' for more details.'}
+        </p>
+        <p>
+          The TGS DMAPI API is licensed as a subproject under the MIT license.
+          {' See the footer of '}
+          <a href={'https://github.com/tgstation/tgstation/blob/master'
+            + '/code/__DEFINES/tgs.dm'}>
+            code/__DEFINES/tgs.dm
+          </a>
+          {' and '}
+          <a href={'https://github.com/tgstation/tgstation/blob/master'
+            + '/code/modules/tgs/LICENSE'}>
+            code/modules/tgs/LICENSE
+          </a>
+          {' for the MIT license.'}
+        </p>
+        <p>
+          {'All assets including icons and sound are under a '}
+          <a href="https://creativecommons.org/licenses/by-sa/3.0/">
+            Creative Commons 3.0 BY-SA license
+          </a>
+          {' unless otherwise indicated.'}
         </p>
       </Section>
     );
@@ -126,6 +329,7 @@ export class Changelog extends Component {
           {header}
           {changes}
           {typeof data === 'string' && <p>{data}</p>}
+          {footer}
         </Window.Content>
       </Window>
     );
