b8c2c5f21c320f472db221d24a41e3ea0ea31c9b
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2022-04-05 19:37:58 +0100

    Pathing fixes

--- tools/ci/check_changelogs.sh
+++ tools/ci/check_changelogs.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 md5sum -c - <<< "9889cdbcb2a2ed07947f1d36a1e0540f *html/changelogs/example.yml"
-python3 tools/ss13_genchangelog.py html/changelogs
+python3 .github/ss13_genchangelog.py html/changelogs
