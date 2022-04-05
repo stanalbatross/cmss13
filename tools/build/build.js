bf32cff146e81bf25dd5a229c622e8c4a838a66e
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2022-04-05 19:51:39 +0100

    Maps

--- tools/build/build.js
+++ tools/build/build.js
@@ -44,10 +44,7 @@ export const WarningParameter = new Juke.Parameter({
 export const DmMapsIncludeTarget = new Juke.Target({
   executes: async () => {
     const folders = [
-      ...Juke.glob('_maps/RandomRuins/**/*.dmm'),
-      ...Juke.glob('_maps/RandomZLevels/**/*.dmm'),
-      ...Juke.glob('_maps/shuttles/**/*.dmm'),
-      ...Juke.glob('_maps/templates/**/*.dmm'),
+      ...Juke.glob('maps/**/*.dmm'),
     ];
     const content = folders
       .map((file) => file.replace('_maps/', ''))
