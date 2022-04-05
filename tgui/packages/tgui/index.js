4ccf238491995bfdf99ca13245a7498399331cbf
Watermelon914 <3052169-Watermelon914@users.noreply.gitlab.com>
2022-04-05 20:08:52 +0100

    Updates

--- tgui/packages/tgui/index.js
+++ tgui/packages/tgui/index.js
@@ -8,6 +8,7 @@
 import './styles/main.scss';
 import './styles/themes/abductor.scss';
 import './styles/themes/cardtable.scss';
+import './styles/themes/spookyconsole.scss';
 import './styles/themes/hackerman.scss';
 import './styles/themes/malfunction.scss';
 import './styles/themes/neutral.scss';
@@ -16,11 +17,10 @@ import './styles/themes/paper.scss';
 import './styles/themes/retro.scss';
 import './styles/themes/syndicate.scss';
 import './styles/themes/wizard.scss';
-import './styles/themes/xeno.scss';
-import './styles/themes/hive_status.scss';
+import './styles/themes/admin.scss';
 
 import { perf } from 'common/perf';
-import { setupHotReloading } from 'tgui-dev-server/link/client';
+import { setupHotReloading } from 'tgui-dev-server/link/client.cjs';
 import { setupHotKeys } from './hotkeys';
 import { captureExternalLinks } from './links';
 import { createRenderer } from './renderer';
